5c5af0d76c29a5be8c6050b29718f3f86388770c
bhanu2931 <bhanuprakashgajula00@gmail.com>
2026-01-28 15:17:07 +0530

    Initial commit - HQLDemo

---
+++ src/main/java/com/klu/Student.java
@@ -0,0 +1,15 @@
+package com.klu;
+
+public class Student {
+
+    private Course course;
+
+    // Setter REQUIRED for XML autowiring
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public void display() {
+        System.out.println("Course: " + course.getCourseName());
+    }
+}
